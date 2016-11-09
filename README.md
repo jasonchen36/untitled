24a7b01c74c7b4e3721f3fbd0e38aeb2799bfe81
mstumpf818 <mstumpf@ellefsontech.com>
2016-11-09 10:48:21 +0000

    core - added mysql db connection; added 'use strict'

---
+++ README.md
@@ -0,0 +1,11 @@
+# dev #
+npm start
+
+
+# note #
+ Organize your Files Around Features, Not Roles
+ 
+ https://blog.risingstack.com/node-hero-node-js-project-structure-tutorial/
+
+# todo #
+https://blog.risingstack.com/node-hero-node-js-unit-testing-tutorial/
--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 console.log('hello world');
-console.log(process.env.HTTP_TIMEOUT);
+console.log(process.env.MYSQL_URL);
+
+const bookshelf = require('./app/database/index');
+
+var User = bookshelf.Model.extend({
+    tableName: 'users'
+});
+
+User.where('id', 1).fetch().then(function(user) {
+    console.log(user.toJSON());
+}).catch(function(err) {
+    console.error(err);
+});
