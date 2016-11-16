b19efaa0e348ff6a9d2a38c6d36eb61f79527646
mstumpf818 <mstumpf@ellefsontech.com>
2016-11-16 13:55:12 +0000

    register - added form validation

--- webapp/javascript/views/user/register.js
+++ webapp/javascript/views/user/register.js
@@ -1,11 +1,40 @@
 (function(){
 
     var $ = jQuery,
-        that = app.views.user.register;
+        that = app.views.user.register,
+        helpers = app.helpers,
+        registerForm = $('#user-register-form'),
+        registerSubmit = $('#register-form'),
+        registerEmailInput = $('#register-email'),
+        registerPasswordInput = $('#register-password'),
+        errorClass = app.helpers.errorClass,
+        disabledClass = app.helpers.disabledClass;
+
+    function submitRegister(){
+        if (!registerSubmit.hasClass(disabledClass)) {
+            var formData = helpers.getFormData(registerForm);
+            helpers.resetForm(registerForm);
+            if (!helpers.isValidEmail(formData.email)) {
+                registerEmailInput.addClass(errorClass);
+            }
+            if (formData.password.length < 1){
+                registerPasswordInput.addClass(errorClass);
+            }
+            if (!helpers.formHasErrors(registerForm)) {
+                console.log('submit register');
+                registerSubmit.addClass(disabledClass);
+            }
+        }
+    }
 
     this.init = function(){
         if ($('#page-user-register').length > 0){
-            console.log('register page');
+
+            //listeners
+            registerForm.on('submit',function(event){
+                event.preventDefault();
+                submitRegister();
+            });
         }
     };
 
