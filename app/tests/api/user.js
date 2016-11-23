a81cfa4d0b628f58c5dc09c864a2cb5f0853a144
mstumpf818 <mstumpf@ellefsontech.com>
2016-11-23 14:53:12 +0000

    tests - added login

--- app/tests/api/user.js
+++ app/tests/api/user.js
@@ -8,6 +8,36 @@ const //packages
 
 describe("User API", function() {
 
+    /**
+     * login
+     */
+    describe("Login", function() {
+
+        it("submit login", function (done) {
+            const options = {
+                method: 'POST',
+                uri: testUrl + '/login',
+                form: {
+                    action: 'api-login',
+                    email: 'test@test.com',
+                    password: 'pass1234'
+                },
+                json: true
+            };
+            request(options, function (error, response, body) {
+                if (debug) {
+                    console.log(body);
+                }
+                expect(response.statusCode).to.equal(200);
+                done(body);
+            });
+        });
+    });
+
+
+    /**
+     * reset password
+     */
     describe("Reset Password", function() {
         
         it("requests password reset", function(done) {
