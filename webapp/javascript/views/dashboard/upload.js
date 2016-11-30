155b001dfb6de0f1f460b6579e5567e55f943ba9
mstumpf818 <mstumpf@ellefsontech.com>
2016-11-30 12:42:31 +0000

    dashboard - added upload select, submit, and cancel buttons; added file type validation

--- webapp/javascript/views/dashboard/upload.js
+++ webapp/javascript/views/dashboard/upload.js
@@ -3,24 +3,35 @@
     var $ = jQuery,
         that = app.views.dashboard.upload,
         helpers = app.helpers,
+        animations = app.animations,
         errorClass = app.helpers.errorClass,
         disabledClass = app.helpers.disabledClass,
         fileUpload,
+        fileUploadSubmit,
+        fileUploadCancel,
+        fileUploadSelect,
         progressBar;
 
     function initializeFileUpload(){
         fileUpload.fileupload({
+            acceptFileTypes: /(\.|\/)(gif|jpe?g|png|pdf|txt|doc|docx|csv|xls|xlsx|ppt|pptx|odt|ott)$/i,
             dataType: 'json',
             add: function (e, data) {
-                data.context = $('<button/>').text('Upload')
-                    .appendTo(document.body)
+                fileUploadSubmit.removeClass(disabledClass);
+                data.context = fileUploadSubmit
                     .click(function () {
-                        data.context = $('<p/>').text('Uploading...').replaceAll($(this));
+                        data.context = fileUploadSubmit.text('Uploading...');
+                        fileUploadCancel.removeClass(disabledClass);
+                        fileUploadSubmit.addClass(disabledClass);
                         data.submit();
                     });
             },
             done: function (e, data) {
-                data.context.text('Upload finished.');
+                fileUploadSubmit.text('Upload finished').addClass(disabledClass);
+                //todo, clear form and or refresh template
+            },
+            cancel: function (e, data) {
+                console.log(data);
             },
             progressall: function (e, data) {
                 var progress = parseInt(data.loaded / data.total * 100, 10);
@@ -32,8 +43,26 @@
     this.init = function(){
         if ($('#dashboard-upload').length > 0) {
             //variables
-            fileUpload = $('#fileupload');
-            progressBar = $('#progress-bar');
+            fileUpload = $('#dashboard-upload-input');
+            progressBar = $('#dashboard-upload-progress');
+            fileUploadCancel = $('#dashboard-upload-cancel');
+            fileUploadSubmit = $('#dashboard-upload-submit');
+            fileUploadSelect = $('#dashboard-upload-select');
+
+            //listeners
+            fileUploadSubmit.on('click', function (event) {
+                event.preventDefault();
+            });
+            fileUploadCancel.on('click', function (event) {
+                event.preventDefault();
+                fileUpload.abort();
+                fileUploadCancel.addClass(disabledClass);
+                fileUploadSubmit.removeClass(disabledClass);
+            });
+            fileUploadSelect.on('click',function(event){
+                event.preventDefault();
+                fileUpload.click();
+            });
 
             //functions
             initializeFileUpload();
