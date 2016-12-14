f3486f123c0a3966822105546f4c65e845d5d670
mstumpf818 <mstumpf@ellefsontech.com>
2016-12-14 14:25:03 +0000

    core - minor cleanup for session state

--- app/services/session.js
+++ app/services/session.js
@@ -114,7 +114,7 @@ session.actionStartUserProfileSession = function(req, token){
                 .then(function (response) {
                     try {
                         response.token = token;
-                        return promise.resolve(session.setUserProfileSession(req, sessionModel.getUserProfileObject(response)));
+                        return promise.resolve(sessionModel.getUserProfileObject(response));
                     } catch(error){
                         if(!error){
                             error = 'Could not create user account';
@@ -136,7 +136,7 @@ session.actionStartUserProfileSession = function(req, token){
                     method: 'GET',
                     uri: process.env.API_URL+'/account/'+accountID,
                     headers: {
-                        'Authorization': 'Bearer '+token
+                        'Authorization': 'Bearer '+userProfileSession.token
                     },
                     body: {
                         name: req.body.firstName,
@@ -148,8 +148,7 @@ session.actionStartUserProfileSession = function(req, token){
                 .then(function (response) {
                     try {
                         userProfileSession.taxReturns = _.map(response.taxReturns, sessionModel.getUserTaxReturns);
-                        session.setUserProfileSession(req, userProfileSession);
-                        return promise.resolve();
+                        return promise.resolve(session.setUserProfileSession(req, userProfileSession));
                     } catch(error){
                         if(!error){
                             error = 'Could not get user\'s tax returns';
