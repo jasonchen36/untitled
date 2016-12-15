70dc8c967738aedf102b3ada1aec28ef01a089ac
Jason <jason.chen@ellefsontech.com>
2016-12-15 09:52:29 -0500

    added alerts, verified postal code, and checkbox is active on click

--- webapp/javascript/views/personal_profile/address.js
+++ webapp/javascript/views/personal_profile/address.js
@@ -14,41 +14,57 @@
         errorClass = app.helpers.errorClass,
         disabledClass = app.helpers.disabledClass;
 
+    function checkPostal(postal) {
+    var regex = new RegExp(/^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ]( )?\d[ABCEGHJKLMNPRSTVWXYZ]\d$/i);
+    if (regex.test(postal.value))
+        return true;
+    else
+        return false;
+    }
+
     function submitAddress(){
         var formData = helpers.getFormData(addressForm);
         helpers.resetForm(addressForm);
         if (addressLine1Input.val().length === 0){
             addressLine1Input.addClass(errorClass);
+            alert("Pleae enter the street address correctly");
         }
         if (cityInput.val().length === 0){
             cityInput.addClass(errorClass);
+            alert("Pleae enter the city correctly");
         }
-        if (postalCodeInput.val().length === 0){
+        if (postalCodeInput.val().length === 0 || checkPostal(postalCodeInput.val())){
             postalCodeInput.addClass(errorClass);
+            alert("Pleae enter the postal code correctly");
         }
         if(provinceInput[0].value === "") {
           postalCodeInput.addClass(errorClass);
+          alert("Pleae enter the province correctly");
         }
         if(residenceInput[0].value === "") {
           postalCodeInput.addClass(errorClass);
+          alert("Pleae enter the province of residence correctly");
         }
         if (addressLine1SpouseInput.val().length === 0){
             addressLine1SpouseInput.addClass(errorClass);
+            alert("Pleae enter the spousal street address correctly");
         }
         if (citySpouseInput.val().length === 0){
             citySpouseInput.addClass(errorClass);
+            alert("Pleae enter the spousal city correctly");
         }
-        if (postalCodeSpouseInput.val().length === 0){
+        if (postalCodeSpouseInput.val().length === 0 || checkPostal(postalCodeSpouseInput.val())){
             postalCodeSpouseInput.addClass(errorClass);
+            alert("Pleae enter the spousal postal code correctly");
         }
         if(provinceSpouseInput[0].value === "") {
           postalCodeInput.addClass(errorClass);
+          alert("Pleae enter the spousal province correctly");
         }
         if(residenceSpouseInput[0].value === "") {
           postalCodeInput.addClass(errorClass);
+          alert("Pleae enter the spousal province of residence correctly");
         }
-        var variable = (!helpers.formHasErrors(addressForm));
-        alert (variable);
         if (!helpers.formHasErrors(addressForm)) {
             addressSubmit.addClass(disabledClass);
             ajax.ajax(
@@ -87,7 +103,7 @@
             provinceSpouseInput = $('#province3');
             residenceSpouseInput = $('#province4');
             postalCodeSpouseInput = $('#address-postal-code-spouse');
-
+            checkbox = $('.checkbox');
 
             //listeners
             addressForm.on('submit',function(event){
@@ -103,6 +119,11 @@
             addressBack.on('click',function(event){
                 event.preventDefault();
                 personalProfile.goToPreviousPage();
+            });
+
+            checkbox.on('click',function(event){
+                event.preventDefault();
+                $(this).toggleClass(helpers.activeClass);
             });
         }
     };
