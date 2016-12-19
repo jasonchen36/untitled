db3166af3ddbccca4b14d31b83b66aca00ad4b69
Jason <jason.chen@ellefsontech.com>
2016-12-19 17:10:56 -0500

    previous for deductions and special scenarios works

--- webapp/javascript/views/personal_profile/deductions.js
+++ webapp/javascript/views/personal_profile/deductions.js
@@ -89,6 +89,76 @@
         }
     }
 
+    function updateDeductions(){
+        if (!deductionsSubmit.hasClass(disabledClass)) {
+            var formData = helpers.getTileFormDataArray(deductionsForm);
+            var sessionData = personalProfile.getPersonalProfileSession();
+            var accountInfo = helpers.getAccountInformation(sessionData);
+
+              return Promise.resolve()
+                    .then(function() {
+                        var promiseArrayPut = [];
+                        var promiseArrayGet = [];
+                        var promiseArrayQuestions = [];
+
+                        var ajaxAnswers = apiservice.getQuestions(sessionData,5);
+
+                        promiseArrayQuestions.push(ajaxAnswers);
+
+                        _.each(formData, function(entry) {
+
+                            var ajaxOne = apiservice.postAnswers(sessionData,
+                                                 entry.taxReturnId, entry);
+
+                            promiseArrayPut.push(ajaxOne);
+
+                            var ajaxTwo = apiservice.getAnswers(sessionData,
+                                                    entry.taxReturnId,5);
+
+                              promiseArrayGet.push(ajaxTwo);
+                        });
+
+                      return Promise.all([Promise.all(promiseArrayPut),
+                            Promise.all(promiseArrayGet),
+                            Promise.all(promiseArrayQuestions)]);
+
+                    })
+                    .then(function(response) {
+
+                        var data = {};
+                        data.accountInfo = accountInfo;
+                        data.taxReturns = formData;
+                        data.taxReturns.questions = response[2];
+
+                        var index = 0;
+                        _.each(data.taxReturns, function(taxReturn){
+                            taxReturn.questions = response[1][index];
+                            _.each(taxReturn.questions.answers, function(question){
+                              question.answer = 0;
+                              question.class = "";
+
+                              if ( !question.text) {
+                                question.answer = 0;
+                                question.class = "";
+                              } else if (question.text === "Yes"){
+                                    question.answer = 1;
+                                    question.class = "active";
+                              }
+
+                            });
+                            index++;
+                        });
+
+                        personalProfile.goToPreviousPage(data);
+                    })
+                    .catch(function(jqXHR,textStatus,errorThrown){
+                        ajax.ajaxCatch(jqXHR,textStatus,errorThrown);
+                        deductionsSubmit.removeClass(disabledClass);
+                    });
+
+        }
+    }
+
     this.init = function(){
         if ($('#personal-profile-deductions').length > 0) {
 
@@ -110,7 +180,7 @@
 
             deductionsBack.on('click',function(event){
                 event.preventDefault();
-                personalProfile.goToPreviousPage();
+                updateDeductions();
             });
         }
     };
--- webapp/javascript/views/personal_profile/special-scenarios.js
+++ webapp/javascript/views/personal_profile/special-scenarios.js
@@ -155,6 +155,142 @@
         }
     }
 
+    function updateSpecialScenarios(){
+
+        if (!specialScenariosSubmit.hasClass(disabledClass)) {
+            var formData = helpers.getTileFormDataArray(specialScenariosForm);
+            var sessionData = personalProfile.getPersonalProfileSession();
+            var accountInfo = helpers.getAccountInformation(sessionData);
+
+                 return Promise.resolve()
+                    .then(function() {
+                        var promiseArrayPut = [];
+                        var promiseArrayGet = [];
+                        var promiseArrayQuestions = [];
+
+                        //todo, question category in variable
+                        var uri = sessionData.apiUrl+ '/questions/product/' + sessionData.productId + '/category/' + 8;
+
+                        var ajaxAnswers = ajax.ajax(
+                            'GET',
+                            uri,
+                            {
+                            },
+                            'json',
+                           {
+                                  'Authorization': 'Bearer '+ accountInfo.token
+                           }
+                        );
+
+                        promiseArrayQuestions.push(ajaxAnswers);
+
+                        _.each(formData, function(entry) {
+
+                            var answerKeys = Object.keys(entry);
+                            var answers = [];
+                            var answerIndex = 0;
+
+                            _.each(entry, function(answer) {
+
+                                    var text= '';
+
+                                if(answer === 1){
+                                    text = 'Yes';
+                                } else if (answer === 0){
+                                    text = 'No';
+                                }
+
+                                if(text.length > 1) {
+
+                                    answers.push(
+                                           {
+                                               questionId: answerKeys[answerIndex],
+                                               text: text
+                                           });
+                                }
+                                answerIndex++;
+
+
+                            });
+
+
+                            var uri = sessionData.apiUrl + '/tax_return/' + entry.taxReturnId + '/answers/';
+
+                            var ajaxOne = ajax.ajax(
+                                   'POST',
+                                    uri,
+                                    {
+                                        'answers': answers
+                                    },
+                                   'json-text',
+                                    {
+                                      'Authorization': 'Bearer '+ accountInfo.token
+                                    }
+
+                                );
+                              promiseArrayPut.push(ajaxOne);
+
+
+
+                            //todo, update with new API route to get tax return with questions and answers in one object
+                            uri = sessionData.apiUrl + '/tax_return/' + entry.taxReturnId + '/answers/category/' + 8;
+
+                            var ajaxTwo = ajax.ajax(
+                                'GET',
+                                uri,
+                                {
+                                },
+                                'json',
+                                 {
+                                  'Authorization': 'Bearer '+ accountInfo.token
+                                 }
+                            );
+
+                            promiseArrayGet.push(ajaxTwo);
+                        });
+
+                      return Promise.all([Promise.all(promiseArrayPut),
+                            Promise.all(promiseArrayGet),
+                            Promise.all(promiseArrayQuestions)]);
+
+                    })
+                    .then(function(response) {
+
+                        var data = {};
+                        data.accountInfo = accountInfo;
+                        data.taxReturns = formData;
+                        data.taxReturns.questions = response[2];
+
+                        var index = 0;
+                        _.each(data.taxReturns, function(taxReturn){
+                            taxReturn.questions = response[1][index];
+                            _.each(taxReturn.questions.answers, function(question){
+                              console.log(JSON.stringify(question.text));
+                              question.answer = 0;
+                              question.class = "";
+
+                              if ( !question.text) {
+                                question.answer = 0;
+                                question.class = "";
+                              } else if (question.text === "Yes"){
+                                    question.answer = 1;
+                                    question.class = "active";
+                              }
+
+                            });
+                            index++;
+                        });
+
+                        personalProfile.goToPreviousPage(data);
+                    })
+                    .catch(function(jqXHR,textStatus,errorThrown){
+                        ajax.ajaxCatch(jqXHR,textStatus,errorThrown);
+                        specialScenariosSubmit.removeClass(disabledClass);
+                    });
+
+        }
+    }
+
     this.init = function(){
         if ($('#personal-profile-special-scenarios').length > 0) {
 
@@ -178,7 +314,7 @@
 
             specialScenariosBack.on('click',function(event){
                 event.preventDefault();
-                personalProfile.goToPreviousPage();
+                updateSpecialScenarios();
             });
         }
     };
